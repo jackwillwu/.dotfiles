556232fef8c0afbf4a76d8283bac97320a619ec8
jackwillwu <jackwill@posteo.net>
2023-02-04 16:27:06 +0800

    first commit

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,34 +1,36 @@
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+# irm get.scoop.sh | iex
 
-irm get.scoop.sh | iex
+# scoop install git
 
-scoop add buckets extra
+# scoop bucket add extras
 
-scoop install git 7zip dark go  mpv SumatraPDF neovim nodejs pandoc python yarn R zotero fzf z
+# scoop install neovim
 
-z 'C:\Users\jackwill\Desktop'
+# scoop install rust R python lua
 
-Invoke-WebRequest -Uri https://media.st.dl.eccdnx.com/client/installer/SteamSetup.exe -OutFile steam.exe
+# scoop install dark SumatraPDF nodejs pandoc python yarn fzf Z rust bat gpgcmake perl pwsh sed sudo screentogif nasm gcc
 
-mkdir C:\Users\jackwill\AppData\Local\nvim
+# cd 'C:\Users\User\Desktop'
 
-Invoke-WebRequest -Uri https://github.com/jackwillwu/config/blob/main/nvim/init.vim D:\init.vim -OutFile C:\Users\wujac\AppData\Local\nvim\init.vim
+# Invoke-WebRequest -Uri https://media.st.dl.eccdnx.com/client/installer/SteamSetup.exe -OutFile steam.exe
 
-Invoke-WebRequest -Uri https://download1.sigma-rt.com/tc/pc/Total_Control_9_0_u11_install_x64.zip -OutFile totalcontorl.exe
+# mkdir C:\Users\User\AppData\Local\nvim
 
-Invoke-WebRequest -Uri https://mullvad.net/media/app/MullvadVPN-2022.5.exe -OutFile mullvad.exe
+# Invoke-WebRequest -Uri https://raw.githubusercontent.com/jackwillwu/.dotfiles/main/nvim/init.vim  -OutFile C:\Users\User\AppData\Local\nvim\init.vim
 
-git clone https://github.com/jackwillwu/mpv-config.git
+# Invoke-WebRequest -Uri https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -OutFile C:\Users\User\scoop\apps\neovim\0.8.3\share\nvim\runtime\autoload\plug.vim
 
-Copy-Item mpv-config\* C:\Users\wujac\scoop\apps\mpv\current\portable_config
+# Invoke-WebRequest -Uri https://mullvad.net/media/app/MullvadVPN-2022.5.exe -OutFile mullvad.exe
+
+# Invoke-WebRequest -Uri https://download.zotero.org/client/release/6.0.20/Zotero-6.0.20_setup.exe -OutFile zotero.exe
+
+# Invoke-Webrequest -Uri https://referrals.brave.com/latest/BraveBrowserSetup.exe -OutFile brave.exe
 
 Invoke-WebRequest -Uri https://economists1.oss-cn-beijing.aliyuncs.com/%E6%8F%92%E4%BB%B6.7z -OutFile plugin.7z
 
 PowerShell -Command "7z x plugin.7z -oplugin"
 PowerShell -Command "rm plugin.7z"
 
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni C:/Users/zamen/scoop/apps/neovim/current/share/nvim/runtime/autoload/plug.vim -Force
 
 Install-Module PSReadLine
 
